5db901cdd25e68c6bb7d6d508e8a63d8c68fbf1c
马凡贺 <827149941@qq.com>
2015-07-01 10:14:31 +0800

    Create UF.java

---
+++ Union-Find/UF.java
@@ -0,0 +1,25 @@
+public class UF
+{
+  private int N;
+
+  UF(int N);                      //initialize union-find data structure with N objects(0 to N-1)
+  void union(int p, int q);       //add connection between p and q
+  boolean connected(int p,int q); //are p and q in the same component?
+  int find(int p);                //conponent identitier for p(0 to N-1)
+  int count();                    //number of component
+
+  public static void main(String[] args)
+  {
+    int N =StdIn.reafInt();
+    UF uf = new UF(N);
+    while(!StdIn.isEmpty())
+    {
+      int p = StdIn.readInt();
+      int q = StdIn.readInt();
+      if (!uf.connected(p,q)) {
+        uf.unin(p,q);
+        StdOut.println(p + " " + q);
+      }
+    }
+  }
+}
