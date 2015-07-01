aacffd254b48ad851e85497d19181b79cf4941ed
马凡贺 <827149941@qq.com>
2015-07-01 15:43:02 +0800

    Update UF.java

--- Union-Find/UF.java
+++ Union-Find/UF.java
@@ -1,10 +1,10 @@
 public class UF
 {
   UF(int N) ;                    //initialize union-find data structure with N objects(0 to N-1)
-  void union(int p, int q);       //add connection between p and q
+  void union(int p, int q);      //add connection between p and q
   boolean connected(int p,int q) //are p and q in the same component?
   int find(int p)                //conponent identitier for p(0 to N-1)
-  int count(){return count;}                    //number of component
+  int count()                    //number of component
 
   public static void main(String[] args)
   {
