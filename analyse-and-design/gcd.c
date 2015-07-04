556d1e4a5cac28f37d0cc81ebe4d50183c2fb405
马凡贺 <827149941@qq.com>
2015-07-04 20:17:18 +0800

    Update gcd.c

--- analyse-and-design/gcd.c
+++ analyse-and-design/gcd.c
@@ -5,20 +5,22 @@ int gcd(int m, int n){
   if(n==0) return m;
   else return gcd(n,m%n);
 }
-int gcd2(int a,int b)
-  int t;
+int gcd2(int a,int b){
+  int t=0;
   while(b!=0){
     t=a%b;
     a=b;
     b=t;
   }
+  return a;
+}
 int main() {
   int a;
   int b;
-  scanf("%d,%d",&a,&b );
+  scanf("%d %d",&a,&b );
   printf("a=%d\n b=%d\n",a,b );
   //printf("b=%d\n",b );
-  int c=gcd(a,b);
+  int c=gcd2(a,b);
   printf("gcd=%d\n",c );
   return 0;
 }
